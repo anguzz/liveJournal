5d32d8a25c95ada35450f9ca46837c3eeb5e1a0e
Angel <santoyox714@gmail.com>
2023-07-25 18:41:20 -0700

    add upcoming

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,8 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 <script>
 	import Concerts from "$lib/pages/Concerts.svelte";
-
+	import Upcoming from "$lib/pages/Upcoming.svelte";
 </script>
 
 
 	<Concerts/>
-	
+	<Upcoming/>
