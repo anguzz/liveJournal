b76509bd09809df25e806004243c29eadbe74abe
Angel Santoyo <santoyox714@gmail.com>
2024-02-24 22:58:36 -0800

    update

--- src/lib/utils/data2.ts
+++ src/lib/utils/data2.ts
@@ -1,15 +1,48 @@
 export const concerts = [
 	{
-		heading: "Enjoy (again)",
-		content: ""
-	},
-	{
 		heading: "TWRP (again)",
 		content: ""
 	},
 	{
 		heading: "Yellow Days",
 		content: ""
-	}
-	
+	},
+	{
+		heading:"The Misfits",
+		content:""
+	},
+	{
+		heading:"Social distortion",
+		content:""
+	},
+	{
+		heading:"Bad Religion",
+		content:""
+	},
+	{
+		heading:"Suicidal Tendencies",
+		content:""
+	},
+	{
+		heading:"T.S.O.L",
+		content:""
+	},
+	{
+		heading:"The Exploited",
+		content:""
+	},
+	{
+		heading:"Adolescents",
+		content:""
+	},	{
+		heading:"The Vandals",
+		content:""
+	},	{
+		heading:"Agent Orange",
+		content:""
+	},
+	{
+		heading:"Black Flag",
+		content:""
+	},
 ];
