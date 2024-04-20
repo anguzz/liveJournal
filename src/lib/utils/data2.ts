9d27d62ddb41f3f2489b13126e11d137334d724a
Angel <santoyox714@gmail.com>
2024-04-20 10:26:45 -0700

    Update data2.ts

--- src/lib/utils/data2.ts
+++ src/lib/utils/data2.ts
@@ -1,13 +1,5 @@
 export const concerts = [
 	{
-		heading: "Enjoy (again)",
-		content: ""
-	},
-	{
-		heading: "TWRP (again)",
-		content: ""
-	},
-	{
 		heading: "Yellow Days",
 		content: ""
 	},
