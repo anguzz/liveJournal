30a8fda256f5dc2dfe090a66031e2f1e9eecd659
Angel <santoyox714@gmail.com>
2024-12-27 15:10:13 -0800

    Update data.ts

--- src/lib/utils/data.ts
+++ src/lib/utils/data.ts
@@ -1,7 +1,7 @@
 export const concerts = [
 	{
 		heading: "311",
-		content: "Their vocals aged very well and sound very refined, great energy"
+		content: "Their vocals aged very well and sound very refined, great energy, saw them a second time for KROQ christmas at kia forum and they sounded amazing once again"
 	},
 	{
 		heading: "Atmosphere",
